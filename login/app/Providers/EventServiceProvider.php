e91007ea58264fd0d204d8672aaf7248b5a17bc6
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-03-06 13:40:42 -0300

    Eventos e Listeners (inicio)

--- login/app/Providers/EventServiceProvider.php
+++ login/app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\HomeEvent;
+use App\Listeners\HomeEventListener;
+use App\Listeners\NewHomeEventListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +21,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        HomeEvent::class => [
+            HomeEventListener::class,
+            NewHomeEventListener::class,
+        ]
+        
     ];
 
     /**
@@ -27,6 +35,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       
     }
 }
